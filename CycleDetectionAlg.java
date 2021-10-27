698adad478e507bee8d0ad498b1b6a7b197c8ed8
Gserrano38 <50222778+Gserrano38@users.noreply.github.com>
2021-10-27 00:18:31 -0700

    implemented inheritance gathered data

--- CycleDetectionAlg.java
+++ CycleDetectionAlg.java
@@ -1,18 +1,25 @@
 
-/**
- * Floyd
- * next steps: create method for regular loop too
- * read and implement Floyd's alg
- */
-public class CycleDetectionAlg {
-    
-    public class Node{ //create a node class with two attributes data and next
+public class CycleDetectionAlg extends Test{ //CycleDetectionAlg subclass inherits the attributes and methods from Test class (superclass)
+
+    public class Node implements IteratedFunction{ //create a node class with two attributes data and next
         int data;
         Node next; //pointer to the next node
 
         public Node(int data){ //constructor
             this.data = data;
             this.next = null;
+        }
+
+        public IteratedFunction next(){ //interface method
+            return next;
+        }
+
+        public boolean equivalent(IteratedFunction node){ //interface method
+            if(node instanceof Node && ((Node)node).data == data){
+                return true;
+            } else{
+                return false;
+            }
         }
     }
 
@@ -51,11 +58,11 @@ public class CycleDetectionAlg {
     //display content of nodes
     public void display() {       
         Node current = head;    //current node point to head
+        System.out.println("Nodes of linked list: ");
         if(head == null) {    
             System.out.println("List is empty");    
             return;    
-        }    
-        System.out.println("Nodes of linked list: ");    
+        }        
         while(current != null) {   
             System.out.print(current.data + " ");    
             current = current.next;    
@@ -63,18 +70,24 @@ public class CycleDetectionAlg {
         System.out.println();    
     } 
 
-    public static void main(String[] args) {    
-            
-        CycleDetectionAlg List = new CycleDetectionAlg();    
-            
-        //Counts the nodes present in the given list    
-        //System.out.println("Count of nodes present in the list: " + List.countNodes()); 
-        //System.out.println("Applying Floyd's Alg: " + Floyd(head));
-        
-        CycleDetectionAlg l = makeList(4,3);
-        //l.display();
-        int[] pair = Floyd(l);
-        System.out.println("Floyd's alg: " + pair[0] + pair[1]);
+    public static void main(String[] args) {  
+        gatherData(100, 100);
+        gatherData(100, 150);
+        gatherData(100, 200);
+        gatherData(100, 250);
+        gatherData(100, 300);
+        System.out.println("---------------------------------------");
+        gatherData(100, 100);
+        gatherData(150, 100);
+        gatherData(200, 100);
+        gatherData(250, 100);
+        gatherData(300, 100);
+        System.out.println("---------------------------------------");
+        gatherData(50);
+        gatherData(100);
+        gatherData(150);
+        gatherData(200);
+        gatherData(250);
     }    
 
     public static CycleDetectionAlg makeList(int l, int k){
@@ -99,66 +112,89 @@ public class CycleDetectionAlg {
     }
 
     //Floyd's Cycle Detection Algorithm
-    public static int[] Floyd(CycleDetectionAlg head){
+    public static int[] Floyd1(CycleDetectionAlg head){
         Node tortoise = head.head;
         Node hare = head.head;
-        int k=1;
-        int l=0;
-        int[] ordered_pair = new int[2];
-        while (tortoise != null && hare != null && hare.next != null){
+        int lambda=1; //length of cycle
+        int mu=0; //index of the first element in cycle (i.e. tail of cycle)
+        int indicator = 0;
+        int[] ordered_pair = new int[3];
+        while (tortoise != null && hare != null && hare.next != null){ //phase one: find cycle
             tortoise = tortoise.next;
+            indicator++;
             hare = hare.next.next;
+            indicator++;
+            indicator++;
             if(tortoise == hare){
-                System.out.println(tortoise.data + " " + hare.data);
                 tortoise = tortoise.next;
-                while(tortoise != hare){
-                    k++;
+                indicator++;
+                while(tortoise != hare){ //phase two: find lambda
+                    lambda++;
                     tortoise = tortoise.next;
+                    indicator++;
                 }
                 tortoise = head.head;
-                while(tortoise != hare){
-                    l++;
+                indicator++;
+                while(tortoise != hare){ //phase three: find mu
+                    mu++;
                     tortoise = tortoise.next;
+                    indicator++;
                     hare=hare.next;
+                    indicator++;
                     
                 }
-                ordered_pair[0]= l;
-                ordered_pair[1]=k;
+                ordered_pair[0]= mu;
+                ordered_pair[1]=lambda;
+                ordered_pair[2]=indicator;
                 return ordered_pair;
             }
         }
-        return new int[]{-1,-1};
+        return new int[]{-1,-1, indicator};
     }
 
+    
+
     //Brent's Cycle Detection Algorithm
-    public static int[] Brent(CycleDetectionAlg head){
+    public static int[] Brent1(CycleDetectionAlg head){
+        int ind=0;
         Node p1 = head.head;
-        Node p2 = head.next;
-        int[] ordered_pair = new int[2];
-        int length = 1;
+        Node p2 = head.head.next;
+        ind++;
+        int[] ordered_pair = new int[3];
+        int lambda = 1; //length of cycle 
         int power = 1;
-        int k=0;
-        while (p1 != p2 && p2 != null){
-            if (length == power){
-                power = power*2; //move p2 in powers of 2
+        int mu=0;        //index of first element in cycle
+        while (p1 != p2 && p2 != null){ //phase one: find cycle and lambda
+            if (lambda == power){
+                power = power*2; 
                 p1 = p2; //reset p1 to previous position of p2
-                length = 0; //reset length
+                lambda = 0;
             }
-            p2 = p2.next;
-            length++;
+            p2 = p2.next; //only hare moves
+            ind++;
+            lambda++;
         }
-        p1 = head.head;
+        if(p2 == null){
+            return new int[]{-1, -1, ind};
+        }
+        p1 = head.head; //prep for phase two
         p2 = head.head;
-        for(int i=1; i<=length;i++){
+        ind++;
+        ind++;
+        for(int i=1; i<=lambda;i++){ 
             p2=p2.next;
+            ind++;
         }
-        while(p1 != p2){
-            k++;
+        while(p1 != p2){ //phase two: find mu
+            mu++;
             p1 = p1.next;
+            ind++;
             p2 = p2.next;
+            ind++;
         }
-        ordered_pair[0]=length;
-        ordered_pair[1]=k;
+        ordered_pair[0]=mu;
+        ordered_pair[1]=lambda;
+        ordered_pair[2]=ind;
         return ordered_pair;
     }
 }
---
+++ IteratedFunction.java
@@ -0,0 +1,4 @@
+public interface IteratedFunction{ //a completely abstract class used to group related methods with empty bodies 
+    public IteratedFunction next();
+    public boolean equivalent(IteratedFunction node);
+}
